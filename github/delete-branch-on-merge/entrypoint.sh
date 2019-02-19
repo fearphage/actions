b4a6730e072517018d74a968a4c24f53efcd9fe2
Phred <fearphage@gmail.com>
2019-02-19 10:50:47 -0600

    simplified logic and removed debug

--- github/delete-branch-on-merge/entrypoint.sh
+++ github/delete-branch-on-merge/entrypoint.sh
@@ -22,28 +22,16 @@ function main {
   merged=$(json .pull_request.merged)
   ref=$(json .pull_request.head.ref)
 
-  >&2 echo "DEBUG: \$action = $action"
-  >&2 echo "DEBUG: \$default_branch = $default_branch"
-  >&2 echo "DEBUG: \$merged = $merged"
-  >&2 echo "DEBUG: \$ref = $ref"
+  if [ "$action" = "closed" ] && [ "$merged" = "true" ] && [ "$ref" != "$default_branch" ]; then
+    >&2 echo "Deleting branch $ref from $(json .pull_request.head.repo.full_name)"
 
-  if [ "$action" = "closed" ] && [ "$merged" = "true" ]; then
-    if [ "$(json .pull_request.base.repo.full_name)" != "$(json .pull_request.head.repo.full_name)" ]; then
-      >&2 echo "No access to remove branches from $(json .pull_request.head.repo.full_name), exiting."
-      exit 0
-    fi
+    curl \
+      --request DELETE \
+      --header "Accept: application/vnd.github.v3+json" \
+      --header "Authorization: token $GITHUB_TOKEN" \
+      "$(json .pull_request.head.repo.url)/git/refs/heads/$ref"
 
-    if [ "$ref" != "$default_branch" ]; then
-      >&2 echo "Deleting branch $ref from $(json .pull_request.head.repo.full_name)"
-
-      # curl \
-        # --request DELETE \
-        # --header "Accept: application/vnd.github.v3+json" \
-        # --header "Authorization: token $GITHUB_TOKEN" \
-        # "$(json .pull_request.head.repo.url)/git/refs/heads/$ref"
-
-      # >&2 echo "Branch deleted successfully!"
-    fi
+    >&2 echo "Branch deleted successfully!"
   fi
 }
 
